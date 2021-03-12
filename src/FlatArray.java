8fe196bb3e27952289ab4916717c07eb55589e11
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-12 16:00:59 +0100

    strassenBahn

---
+++ src/FlatArray.java
@@ -0,0 +1,15 @@
+public class FlatArray {
+    public static void main(String[] args) {
+        int[][] my_array1=new int[3][3];
+        my_array1= new int[][]{{1, 2, 3},
+                               {4, 5, 6}};
+
+        int[] my_arrayResult= new int[6];
+
+        my_arrayResult= getFlatArray(my_array1);
+
+    }
+    public static int[] getFlatArray(int[][] numbers){
+
+    };
+}
